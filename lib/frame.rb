ee22fccd2b93c912b2cede862588d63cd68d4e2a
bullhornfixie <dawson.jon@hotmail.co.com>
2021-02-14 18:00:20 +0000

    spare function not adding points correctly

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,16 +1,9 @@
 
 class Frame 
-  attr_reader :score
-
-  def initialize
-    @score = 0 
-  end 
 
   def roll
     puts "player enter pins knocked down"
     pins_down = gets.chomp.to_i 
-
-    @score += pins_down
 
     pins_down 
   end
--- lib/game.rb
+++ lib/game.rb
@@ -10,7 +10,7 @@ class Game
   end
 
   def start
-    while @frame_count != 5
+    while @frame_count != 6
       frame = Frame.new 
       roll1 = frame.roll
 
@@ -21,24 +21,19 @@ class Game
         roll2 = frame.roll 
       end 
 
-      score = @total_score += (roll1 + roll2)
+      @total_score += (roll1 + roll2)
       puts "end of frame"
       @frame_count +=1
 
-      self.bonus_log(roll1, roll2)
-
-      puts @score_card << {"frame_num"=>@frame_count, "roll1"=>roll1, "roll2" => roll2, 
-                           "score"=> score, "spare" => @spare}        
+      self.spare_log(roll1, roll2)
+      puts @score_card << {"frame_num"=>@frame_count, "roll1"=>roll1, "roll2" => roll2, "score"=> @total_score, "spare" => @spare} 
+      puts @total_score
     end
   end
 
-  def bonus_log(roll1, roll2)
+  def spare_log(roll1, roll2)
     roll1 != 10 && roll1 + roll2 == 10 ? @spare = true : @spare = false 
   end
-    
-  def end 
-    @score_card.map { |s| s['score']}.reduce(0, :+)
-  end 
 
 end
 
