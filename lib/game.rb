c5bda097090d893efeccef20a3192301e9d31c60
bullhornfixie <dawson.jon@hotmail.co.com>
2021-02-15 12:55:59 +0000

    combined functions into single class

--- lib/game.rb
+++ lib/game.rb
@@ -1,42 +1,40 @@
-require './lib/frame'
-
 class Game 
 
   def initialize
     @frame_count = 0
-    @score_card = []
     @total_score = 0
-    @spare = false 
+    @spare_bonus = false 
   end
 
   def start
-    while @frame_count != 6
-      frame = Frame.new 
-      roll1 = frame.roll
-
-      if roll1 == 10
-        puts "strike!"
-        roll2 = 0 
-      else 
-        roll2 = frame.roll 
-      end 
-
-      @total_score += (roll1 + roll2)
-      puts "end of frame"
+    while @frame_count != 1
+      roll1 = self.roll
+      roll2 = self.roll unless roll1 == 10
+      self.score(roll1, roll2, @spare_bonus)
+      self.spare(roll1, roll2)
       @frame_count +=1
-
-      self.spare_log(roll1, roll2)
-      puts @score_card << {"frame_num"=>@frame_count, "roll1"=>roll1, "roll2" => roll2, "score"=> @total_score, "spare" => @spare} 
-      puts @total_score
+      self.end 
     end
   end
 
-  def spare_log(roll1, roll2)
-    roll1 != 10 && roll1 + roll2 == 10 ? @spare = true : @spare = false 
+  def roll 
+    puts "player enter pins knocked down"
+    pins_down = gets.chomp.to_i 
+  end
+  
+  def score(roll1, roll2=0, spare_bonus)
+    roll1 = roll1 * 2 unless spare == false 
+    @total_score += (roll1 + roll2)
   end
 
+  def spare(roll1, roll2)
+    roll1 != 10 && roll1 + roll2 == 10 ? @spare_bonus = true : @spare_bonus = false 
+  end
+
+  def end 
+    puts "end of frame and your score is #{@total_score}"
+  end
 end
 
-kingpin = Game.new
-kingpin.start 
+
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,21 @@
 require './lib/game'
 
 describe Game do 
+  let(:kingpin) { Game.new }
 
-  describe '.start' do
-    it 'asks the user to input pins knocked down' do 
-      kingpin = Game.new 
-      kingpin.start 
-    end
+  it 'creates a new game of bowling with 10 frames' do
+    expect(kingpin).to respond_to :start
+    expect(kingpin).to respond_to :roll 
+    expect(kingpin).to respond_to :score
+    expect(kingpin).to respond_to :spare
+    expect(kingpin).to respond_to :end 
   end
 
-end
+  describe '.spare' do 
+    it 'checks if a frame results in a spare bonus and changes spare_bonus to true' do 
+      frame = kingpin.spare(2, 8)
+      expect(frame).to eq true 
+    end
+  end 
+
+end 
